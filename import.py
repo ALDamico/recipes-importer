b8468158e49a8ec8413aa76207a74fd42638454b
Andrea Luciano Damico <aldamico90@gmail.com>
2020-06-07 16:55:23 +0200

    Added recipes input files

---
+++ import.py
@@ -0,0 +1,5 @@
+import psycopg2
+from nltk.corpus import brown
+import json
+
+FILES = ["input_files/recipes_raw_nosource_ar.json", "input_files/recipes_raw_nosource_fn.json", "input_files/recipes_raw_nosource_epi.json"]
